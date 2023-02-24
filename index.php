2f3c5422b204f2bab79f3f858e17e95e1aec113f
Cesar Calderon <cesarsiri@live.com>
2023-02-24 08:27:28 -0500

    commit

--- index.php
+++ index.php
@@ -1,40 +1,5 @@
 <?php include("layout/header.php");?>  
 <style>
-.like-button{
-  background-color: black;
-  color: white;
-  padding: 4px;
-  width: 81px;
-  border: solid 1px gray;
-  transition: background-color ease 0.3s;
-}
-
-.like-button:hover{
-    background-color: rgb(48, 48, 48);
-}
-
-.emoji-content{
-    background-color: white;
-    border: solid 1px #3f3c3c;
-    border-radius: 11pc;
-    padding: 1px;
-    width: 60%;
-    position: relative;
-    right: -19px;
-}
-.emoji-display{
-  
-    width: 31px;
-    margin-bottom: -2px;
-}
-
-.show-emojis{
-    background-color: blue;
-}
-
-.show-emojis:hover .emoji-content{
-   background-color:red;
-    }
 
 </style>
 
