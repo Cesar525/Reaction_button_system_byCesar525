2a8ca7f427036d799ba9337a2113e7e624a711f7
Cesar <Cesarsiri@live.com>
2023-02-24 16:51:58 -0800

    commited

--- index.php
+++ index.php
@@ -1,27 +1,38 @@
 <?php include("layout/header.php");?>  
-<style>
-
-</style>
 
 
 <div style="background-color:white;padding: 11px;">
 
 <div style="background-color: #1e1e49;width: 361px;padding: 6px;border-radius: 11px;">
 <div class="emoji-content">
-    <img class="emoji-display" src="emojis/thumbsup.png" alt="">
-    <img class="emoji-display" src="emojis/love.png" alt="">
-    <img class="emoji-display" src="emojis/laughing.png" alt="">
-    <img class="emoji-display" src="emojis/sad.png" alt="">
-    <img class="emoji-display" src="emojis/angry.png" alt="">
-    <img class="emoji-display" src="emojis/openmouth.png" alt="">
+<font>helo</font>
+   
 </div>    
 <font style="color:white;">0 Reactions</font>&nbsp; <button class="like-button"><font class="show-emojis">Like</font></button>&nbsp; <font color="white">jose reacted and 25 others...</font>
 </div>
 
-
 </div>
 
 
+
+
+
+
+<style>
+.hide {
+  display: none;
+}
+    
+.myDIV:hover + .hide {
+  display: block;
+  color: red;
+}
+</style>
+
+<h2>Display an Element on Hover</h2>
+
+<div class="myDIV">Hover over me.</div>
+<div class="hide">I am shown when someone hovers over the div above.</div>
 
 
 <?php include("layout/footer.php");?>
