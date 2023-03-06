97ab97075ffc83de715517e4db6f62eeb443e7e4
Cesar <Cesarsiri@live.com>
2023-03-06 07:48:22 -0800

    commited by Cesar

--- index.php
+++ index.php
@@ -3,101 +3,12 @@ include("cesar525/header.php");
 include("cesar525/engine/init.php");
 
 ?>
-
-
-<style>
-.like-main-container {
-    background-color: #0d0d0d;
-    padding: 11px;
-    border-radius: 11px;
-    width: 310px;
-}
-
-.like-button {
-
-    color: white;
-    padding: 4px;
-    width: 81px;
-    border: solid 1px gray;
-    cursor: pointer;
-    border-radius: 11px;
-}
-
-.like-button-effect {
-    background-color: black;
-    transition: background-color ease 0.3s;
-}
-
-.like-button-effect:hover {
-    background-color: #3e3b3b;
-}
-
-.emojis-container {
-    display: none;
-    color: red;
-    background-color: #666666;
-    width: fit-content;
-    height: fit-content;
-    border: 1px solid gray;
-    position: absolute;
-    max-width: 400px;
-    padding-left: 6px;
-    border-radius: 3px;
-    animation: fadeIn 0.3s;
-    width: 198px;
-
-}
-
-.emojis-container:hover {
-    display: block;
-}
-
-.like-button:hover+.emojis-container {
-    display: block;
-}
-
-.emojis-button {
-    padding: 3px;
-    width: 21px;
-    margin-bottom: -4px;
-    margin-left: -2px;
-    border: solid 1px #6d6d6d00;
-    margin-right: 6px;
-    border-radius: 9px;
-    background-color: transparent;
-    transition: background-color ease 0.3s;
-}
-
-.emojis-button:hover {
-    background-color: #35333369;
-    cursor: pointer;
-}
-
-.click-to-check-likes {
-    color: #b5b5b5;
-    transition: color ease 0.3s;
-}
-
-.click-to-check-likes:hover {
-    color: #c78e54;
-    cursor: pointer;
-}
-
-@keyframes fadeIn {
-    0% {
-        opacity: 0;
-    }
-
-    100% {
-        opacity: 1;
-    }
-}
-</style>
+    <link rel="stylesheet" href="cesar525/emoji_style.css">
 
 <?php
 $emojis_reaction = [
     "<div><img class='emojis-button' style='width:10px;' src='cesar525/emojis/thumbsup.png' alt='nothing'> <font style='font-size: 10px;margin-left: -9px;position: relative;top: -1px;'> Like</font></div>",
-    "<div><img class='emojis-button' style='width:10px;' src='cesar525/emojis/thumbsup.png' alt='nothing'> <font style='font-size: 10px;margin-left: -9px;position: relative;top: -1px;color: #6969ff;'> Liked</font></div>",
+    "<div><img class='emojis-button' style='width:10px;' src='cesar525/emojis/thumbsup.png' alt='nothing'> <font style='font-size: 10px;margin-left: -9px;position: relative;top: -1px;color: #6969ff;'><font class='glow'> Liked</font></font></div>",
 "<div><img class='emojis-button' style='width:10px;' src='cesar525/emojis/love.png' alt='nothing'> <font style='font-size: 10px;margin-left: -9px;position: relative;top: -1px;'> Love</font></div>",
    "<div><img class='emojis-button' style='width:10px;' src='cesar525/emojis/openmouth.png' alt='nothing'> <font style='font-size: 10px;margin-left: -9px;position: relative;top: -1px;color: #dfdf7f;'> Wow</font></div>",
 "<div><img class='emojis-button' style='width:10px;' src='cesar525/emojis/angry.png' alt='nothing'> <font style='font-size: 10px;margin-left: -9px;position: relative;top: -1px;color: #ff5e5e;'> Angry</font></div>" ,
@@ -148,8 +59,7 @@ if($checking_react){
         <!-- showing emijs -->
         <button id="showemojis<?php echo $post_id;?>" onclick="likeUnlike()" style="display:inline-block;"
             class="like-button like-button-effect">
-           <?php// echo $emojis_reaction[$current]; ?>
-            <div id="one<?php echo $post_id;?>"></div>
+           <?php echo $emojis_reaction[$current]; ?>
         </button>
 
 
@@ -174,6 +84,8 @@ if($checking_react){
 
         <!-- result data show here -->
     </div>
+    <div id="one<?php echo $post_id;?>"></div>
+
 </div>
 
 <?php } ?>
