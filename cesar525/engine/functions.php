f3836c0f6e0701ae8f38a9d086e04ba807cc0ac1
Cesar <Cesarsiri@live.com>
2023-03-07 06:32:05 -0800

    commited by cesar

--- cesar525/engine/functions.php
+++ cesar525/engine/functions.php
@@ -14,12 +14,27 @@ function countingPostReactions($post_id, $conn){
 if($coutning_total_react){
     if(mysqli_num_rows($coutning_total_react) == 0){
         $react_count = 0;
-        echo 'its zero';
+    return $react_count;
     }else{
-        $num_rows = mysqli_num_rows($coutning_total_react);
-       return $num_rows;
+        $react_count = mysqli_num_rows($coutning_total_react);
+       return $react_count;
     }
 }
+}
+
+
+// this function need more work 
+function likeMessage($user_id, $post_id, $count){
+    if(true){ // you and others react to it
+echo 'You and '.$count.' others reacted this. ';
+    }
+    if(true){ // others react toit but you
+    echo $count. 'people reacted to this.';
+    }
+    if(true){ // when you are the only first one reacting to it.
+        echo 'You reacted to this.';
+        
+    }
 }
 
 ?>
