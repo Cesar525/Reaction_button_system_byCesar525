ed72172ed38d19381d7d9d6184996aa4db1ba4c1
Cesar Calderon <cesarsiri@live.com>
2023-03-13 21:20:01 -0400

    commited by cesar

--- cesar525/modal.php
+++ cesar525/modal.php
@@ -1,21 +1,26 @@
 
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+<link rel="stylesheet" href="cesar525/modal_style.css">
+
 
 <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-black">Fade In Modal</button>
 
 <div id="id01" class="w3-modal w3-animate-opacity">
-  <div class="w3-modal-content w3-card-4">
-    <header class="w3-container w3-teal"> 
-      <span onclick="document.getElementById('id01').style.display='none'" 
+  <div class="w3-modal-content w3-card-4" style="border-radius: 0;background-color: #090909;border: ;">
+    <header style="background-color: #1a1a1a;border-radius: 0px;height: 20px;border: solid 1px #383838;"> 
+      <span style="display:inline-block;" onclick="document.getElementById('id01').style.display='none'" 
       class="w3-button w3-large w3-display-topright">&times;</span>
-      <h2>Modal Header</h2>
+     <div style="display:inline-block;width: 97%;"><center ><font>Who reacted to this?</font></center> </div>
     </header>
     <div class="w3-container">
+    <div style="padding: 11px;"> 
+    <p>Some text..</p>
+      <p>Some text..</p>
+      <p>Some text..</p>
+      <p>Some text..</p>
       <p>Some text..</p>
       <p>Some text..</p>
     </div>
-    <footer class="w3-container w3-teal">
-      <p>Modal Footer</p>
-    </footer>
+   </div> 
   </div>
 </div>
