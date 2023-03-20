432a902853bf9409c42203c6c915179cf1ba87bb
Cesar <Cesarsiri@live.com>
2023-03-20 13:54:44 -0700

    commited by cesar

--- installing.php
+++ installing.php
@@ -2,12 +2,12 @@
 //include this these headers
 include("cesar525_likebutton/engine/init.php");
 
-//include these variables inside the while loop set them up with your own info.
+//include these variables inside the while loop set them up with your own info along with the display function .
+
 $post_id = 1;
 $loggedin_user_id = 234;
 $account_SQL = NULL;
 $account_user_name_column = NULL;
-
 
 //display the button fucntion
 echo'<center>';
